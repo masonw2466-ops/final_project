d5efb498be16c236ea6feb3fbce2fb33594250ff
masonw2466-ops <masonw2466@gmail.com>
2025-11-13 11:32:27 -0700

    Added login checker to login.py and added comments to all files

--- final_project.py
+++ final_project.py
@@ -8,34 +8,44 @@ class GymInterface(staff_status):
         self.root.geometry("500x500")
         self.staff_status = staff_status
 
+        # Lets employee add gym member
         self.button_add_member = tk.Button(root, text="Add Member", command=self.add_member)
         self.button_add_member.pack(pady=10)
 
+        # Lets staff edit member info
         self.button_edit_member = tk.Button(root, text="Edit Member", command=self.edit_member)
         self.button_edit_member.pack(pady=10)
 
+        # Lets staff edit or see class schedule
         self.button_class_schedule = tk.Button(root, text="Class Schedule", command=self.class_schedule)
         self.button_class_schedule.pack(pady=10)
 
+        # Lets staff see maintenance logs
         self.button_maintenance_logs = tk.Button(root, text="Maintenance Logs", command=self.maintenance_logs)
         self.button_maintenance_logs.pack(pady=10)
 
+        # Adds extra options for the manager
         if self.staff_status == "Manager":
             self.button_manage_staff = tk.Button(root, text="Manage Staff", command=self.manage_staff)
             self.button_manage_staff.pack(pady=10)
 
+    # Adds the method for adding members
     def add_member(self):
         pass
 
+    # Adds method for editing members
     def edit_member(self):
         pass
     
+    # Adds method for the class schedules
     def class_schedule(self):
         pass
     
+    # Allows you to change and update maintenance logs
     def maintenance_logs(self):
         pass
 
+    # Allows manager to add and fire staff
     def manage_staff(self):
         pass
 
--- login.py
+++ login.py
@@ -6,15 +6,41 @@ class Login:
         self.root.title("Login")
         self.root.geometry("500x500")
 
+        # Adds the prompt asking for ID
         self.label_prompt_id = tk.Label(root, text="Enter Employee ID")
         self.label_prompt_id.pack(pady=10)
 
+        # Adds the box to place ID
         self.entry_id = tk.Entry(root)
         self.entry_id.pack(pady=10)
 
-
+        # Adds prompt asking for passcode
         self.label_prompt_passcode = tk.Label(root, text="Enter Passcode")
         self.label_prompt_passcode.pack(pady=10)
         
+        # Place passcode here
         self.entry_passcode = tk.Entry(root)
         self.entry_passcode.pack(pady=10)
+
+        # Submits informaiton and calls a checker
+        self.button_submit = tk.Button(root, text="Submit", command="check_login")
+        self.button_submit.pack(pady=10)
+
+        # Left empty to start but will display message if done wrong
+        self.label_output = tk.Label(root, text="")
+        self.label_output.pack(pady=10)
+
+    # Method for checking that the login exists
+    def check_login(self):
+        employee_id = self.entry_id.get()
+        password = self.entry_passcode()
+        if (employee_id.strip()) or (password) == "":
+            self.label_output.config(text="Invalid ID or password")
+        
+        ''' Right here we want to add an if or 
+        omehting that will check if the id and 
+        password are in a dictionary or something
+        and then decide whether to send a value of 
+        manager or employee. Or should we add a 
+        database? It would also need to send them to
+        the main page'''
